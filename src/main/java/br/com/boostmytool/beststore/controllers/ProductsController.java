e344dad42ce87616037729a55612c4d163e0bdcb
Lucas Lima <lucaserolima@gmail.com>
2024-01-24 19:40:16 -0400

    Created method 'showProductsList()

--- src/main/java/br/com/boostmytool/beststore/controllers/ProductsController.java
+++ src/main/java/br/com/boostmytool/beststore/controllers/ProductsController.java
@@ -1,13 +1,25 @@
 package br.com.boostmytool.beststore.controllers;
 
+import br.com.boostmytool.beststore.models.Product;
 import br.com.boostmytool.beststore.services.ProductsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
 public class ProductsController {
     @Autowired
     private ProductsRepository productsRepository;
+
+    @GetMapping({"", "/"})
+    public String showProductsList(Model model) {
+        List<Product> products = productsRepository.findAll();
+        model.addAttribute("products", products);
+        return "products/index";
+    }
 }
