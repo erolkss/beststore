e599225cfa75f27297316bf702a79c29aa867bc2
Lucas Lima <lucaserolima@gmail.com>
2024-01-24 19:33:42 -0400

    Created 'ProductsRepository'

---
+++ src/main/java/br/com/boostmytool/beststore/services/ProductsRepository.java
@@ -0,0 +1,7 @@
+package br.com.boostmytool.beststore.services;
+
+import br.com.boostmytool.beststore.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductsRepository extends JpaRepository<Product, Integer> {
+}
