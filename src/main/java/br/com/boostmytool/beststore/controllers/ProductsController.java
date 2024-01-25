26621860d51640aebc5b34ee4c20c5de7b507031
Lucas Lima <lucaserolima@gmail.com>
2024-01-25 16:13:10 -0400

    Add 'products/index' to show the product list

--- src/main/java/br/com/boostmytool/beststore/controllers/ProductsController.java
+++ src/main/java/br/com/boostmytool/beststore/controllers/ProductsController.java
@@ -3,6 +3,7 @@ package br.com.boostmytool.beststore.controllers;
 import br.com.boostmytool.beststore.models.Product;
 import br.com.boostmytool.beststore.services.ProductsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/products")
 public class ProductsController {
     @Autowired
     private ProductsRepository productsRepository;
 
-    @GetMapping({"", "/"})
+    @GetMapping({"","/"})
     public String showProductsList(Model model) {
         List<Product> products = productsRepository.findAll();
         model.addAttribute("products", products);
